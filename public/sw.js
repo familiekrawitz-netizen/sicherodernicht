9471f3feb17a86d015f73da73990483e264734df
Peter Krawitz <familiekrawitz@icloud.com>
2026-04-22 11:20:08 +0200

    Add spacing before fun ratings

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'sicherodernicht-v43';
-const APP_SHELL = ['/', '/index.html', '/sicherheit.html', '/styles.css?v=64', '/app.js?v=69', '/manifest.json?v=48', '/assets/biss-logo.svg'];
+const CACHE_NAME = 'sicherodernicht-v44';
+const APP_SHELL = ['/', '/index.html', '/sicherheit.html', '/styles.css?v=65', '/app.js?v=69', '/manifest.json?v=48', '/assets/biss-logo.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
